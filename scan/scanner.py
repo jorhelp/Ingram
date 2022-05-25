8933fd352ecf3e452580c047a529693074ef242c
jorhelp <2373911050@qq.com>
2022-05-25 19:43:22 +0800

    Fixed memory explosion bug

--- scan/scanner.py
+++ scan/scanner.py
@@ -8,8 +8,8 @@ from multiprocessing import Lock
 CWD = os.path.dirname(__file__)
 sys.path.append(os.path.join(CWD, '..'))
 from scan.modules import *
-from utils.net import get_all_ip
-from utils.base import multi_thread, multi_process, process_bar, save_res
+from utils.net import get_all_ip, get_ip_seg_len
+from utils.base import multi_thread, process_bar, save_res
 
 
 class Base:
@@ -43,8 +43,8 @@ class CameraScanner(Base):
     def __init__(self, in_file: str, out_file: str) -> None:
         super().__init__(in_file, out_file)
         self.scanner_name = 'camera scanner'
-        self.ip_list = []
         self.lock = Lock()
+        self.ip_list = []
         self.total = 0
         self.found = 0
         self.done = 0
@@ -56,13 +56,9 @@ class CameraScanner(Base):
     def _get_ip(self):
         with open(self.in_file, 'r') as f:
             for line in f:
-                if line.strip():
-                    if not line.startswith('#'):
-                        if '-' in line or '/' in line:
-                            self.ip_list.extend(get_all_ip(line.strip()))
-                        else:
-                            self.ip_list.append(line.strip())
-        self.total = len(self.ip_list)
+                if line.strip() and not line.startswith('#'):
+                    self.total += get_ip_seg_len(line.strip()) if '-' in line or '/' in line else 1
+                    self.ip_list.append(line.strip())
 
     def _step(self, *args, **kwargs):
         with self.lock:
@@ -70,17 +66,18 @@ class CameraScanner(Base):
                 self.found += 1
             self.bar(self.total, self.done + 1, self.found, timer=True, start_time=self.start_time)
 
-    def scan(self, ip):
-        for mod in self.modules:
-            found = False
-            try:
-                res = mod(ip)
-                if res[0]:
-                    found = True
-                    save_res(self.out_file, [ip] + res[1:])
-            except Exception as e: pass  # print(e)
-            finally: self._step(found=found)
-        with self.lock: self.done += 1
+    def scan(self, ip_term):
+        for ip in get_all_ip(ip_term):
+            for mod in self.modules:
+                found = False
+                try:
+                    res = mod(ip)
+                    if res[0]:
+                        found = True
+                        save_res(self.out_file, [ip] + res[1:])
+                except Exception as e: pass  # print(e)
+                finally: self._step(found=found)
+            with self.lock: self.done += 1
 
 
     def __call__(self, args):
@@ -102,4 +99,5 @@ class CameraScanner(Base):
             if args.cve_2021_36260: self.modules.append(cve_2021_36260)
             if args.cve_2020_25078: self.modules.append(cve_2020_25078)
             if args.cve_2021_33044: self.modules.append(cve_2021_33044)
+        
         multi_thread(self.scan, self.ip_list, processes=args.th_num)
