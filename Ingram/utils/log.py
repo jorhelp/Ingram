7cf0a853a6b4ccd030e87f3255b597df94955dc8
jorhelp <2373911050@qq.com>
2022-08-13 01:01:51 +0800

    stash

--- Ingram/utils/log.py
+++ Ingram/utils/log.py
@@ -5,11 +5,12 @@ from loguru import logger
 logger.remove(handler_id=None)  # do not print to terminal
 
 def no_debug(record):
-    """if debug is False, then filter error msg"""
-    return record['level'].name != 'ERROR'
+    """if debug is False, then filter error and debug msg"""
+    return record['level'].name != 'ERROR' and record['level'].name != 'DEBUG'
 
-_formatter = "[{time:YYYY-MM-DD HH:mm:ss}]({level}) From {module}.{function} : {message}"
-if debug:
-    trace = logger.add('test.log', format=_formatter)
-else:
-    trace = logger.add('test.log', format=_formatter, filter=no_debug)
+_formatter = "[{time:YYYY-MM-DD HH:mm:ss}][{level}][{module}.{function}] {message}"
+# if debug:
+#     trace = logger.add('test.log', format=_formatter)
+# else:
+#     trace = logger.add('test.log', format=_formatter, filter=no_debug)
+trace = logger.add('test.log', format=_formatter, filter=no_debug)
